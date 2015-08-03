c31c65174203b742c381808b23987ef888f43791
unknown <Mun-Kiat.Kok@APSTCCBU0001.ap.didata.local>
2015-08-03 17:39:06 +0800

    'hooktest'

--- polls/models.py
+++ polls/models.py
@@ -5,10 +5,9 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
