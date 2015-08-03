18d315130dc45333a8329ff2c82d5a36add2b202
unknown <Mun-Kiat.Kok@APSTCCBU0001.ap.didata.local>
2015-08-03 18:49:36 +0800

    added tests

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    def __unicode__(self):              # __unicode__ on Python 2
+        return self.question_text
+
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
